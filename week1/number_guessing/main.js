17e149c3649fb1d9e1c0c3735a1f8576f7705aa1
Sonny-2k <Sonny-2k@users.noreply.github.com>
2020-09-11 01:27:22 +0700

    update number_guessing

--- week1/number_guessing/main.js
+++ week1/number_guessing/main.js
@@ -1,31 +1,49 @@
 var target = Math.floor(Math.random() * 100) + 1
 var pastGuesses = ''
-var count = 10
+var guessRemaining = 10
+var button = document.getElementById('guessButton')
 
-function myFunction() {
+const reset = () => {
+    let loadingTimer = 5
+    setInterval(() => {
+        if(loadingTimer <= 0) {
+            clearInterval()
+            location.reload()
+        } else {
+            document.getElementById("notification").innerHTML = `Reload game after ${loadingTimer}`
+        }
+        loadingTimer -= 1
+    },1000)
+}
+
+button.addEventListener('click', function() {
     var yourInput = document.getElementById("yourNumber").value
     //console.log(target)
-    count--
+    guessRemaining--
 
     if(yourInput == target) {
         document.getElementById("message").innerHTML = "Congratulations!! You guessed correctly."
         document.getElementById("message").style.background = "green"
+        document.getElementById("notification").innerHTML = "---------------------"
+        reset()
     }
     else if(yourInput < target) {
-        document.getElementById("message").innerHTML = "Your guess was too low, guess higher. You have " + count +" left"
+        document.getElementById("message").innerHTML = `Your guess was too low, guess higher. You have ${guessRemaining} left`
         document.getElementById("message").style.background = "red"
     }
     else if(yourInput > target) {
-        document.getElementById("message").innerHTML = "Your guess was too high, guess lower. You have " + count +" left"
-        document.getElementById("message").style.background = "red"
+        document.getElementById("message").innerHTML = `Your guess was too high, guess lower. You have ${guessRemaining} left`
+        document.getElementById("message").style.background = "tomato"
     }
 
-    if(count == 0) {
-        alert("You lose! The right choice is " + target)
-        location.reload()
+    if(guessRemaining == 0) {
+        document.getElementById("message").innerHTML = `You lose! The right choice is ${target}`
+        document.getElementById("message").style.background = 'tomato'
+        document.getElementById("guessButton").disabled = true
+        reset()
     }
 
     pastGuesses = pastGuesses + yourInput + '<br>'
     document.getElementById("pastGuesses").innerHTML = pastGuesses
     document.getElementById("pastGuesses").style.textAlign = 'center'
-}
+})
